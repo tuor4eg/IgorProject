e09b267c6b776775a37368f647c7e084c677c6e2
tuor4eg <tuor.rauchris@gmail.com>
2018-10-26 15:11:40 +0300

    initial project version

---
+++ index.js
@@ -0,0 +1,79 @@
+const mysql = require('mysql');
+const Express = require('express');
+const bodyParser = require("body-parser");
+const crypto = require('crypto');
+
+
+const token = '123';
+
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '123',
+  database : 'node_test'
+});
+
+const app = new Express;
+
+const checkToken = (req, res, next) => {
+    if (req.headers.token === token) {
+        return next();
+    }
+    return res.end('fuck off');
+};
+
+const hash = pass => crypto.createHash('md5').update(pass).digest('hex');
+
+app.use(checkToken);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/data', (req, res) => {
+    connection.query('select * from users_test', (error, results) => {
+        if (error) throw error;
+        res.send(JSON.stringify(results));
+    });
+  });
+
+app.post('/data/login', (req, res) => {
+    const {login, pass} = req.body;
+    const md5 = hash(pass);
+    const makeQuery = `select name, role from users where login = '${login}' and md5password = '${md5}'`;
+    connection.query(makeQuery, (error, results) => {
+        if (error) throw error;
+        res.send(JSON.stringify(results));
+    })
+})
+
+app.post('/data/post', (req, res) => {
+    const {name, sum, text} = req.body;
+    const makeQuery = `insert into users_test (name, sum, text) values ('${name}', '${sum}', '${text}')`;
+    connection.query(makeQuery, (error, results) => {
+        if (error) throw error;
+        res.send(results);
+    });
+});
+
+app.patch('/data/patch/:id', (req, res) => {
+    const { id } = req.params;
+    const {name, sum, text} = req.body;
+    const makeQuery = `update users_test set name = '${name}', sum = '${sum}', text = '${text}' where id = '${id}'`;
+    connection.query(makeQuery, (error, results) => {
+        if (error) throw error;
+        res.send('ok');
+    });
+});
+
+app.delete('/data/delete/:id', (req, res) => {
+    console.log('try');
+    const { id } = req.params;
+    const makeQuery = `delete from users_test where id = ${id}`;
+    connection.query(makeQuery, (error, results) => {
+        if (error) throw error;
+        res.send('ok');
+    });
+})
+  
+app.listen(3000, () => {
+    console.log('Example Express app listening on port 3000!');
+  });
